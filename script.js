b2ec817d1e99d0e1ea3d50b9ee0c67d8ac44b35e
sriram9553 <moparthi.sriram.124@gmail.com>
2021-08-21 00:37:01 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+var currentActive = 1;
+next.addEventListener("click", () => {
+    currentActive++
+    if (currentActive > circles.length) {
+        currentActive = circles.length
+    }
+   
+    result();
+})
+prev.addEventListener("click", () => {
+    currentActive--
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    result();
+})
+
+function result() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add('active');
+        } else {
+            circle.classList.remove('active');
+        }
+    });
+
+    const actives = document.querySelectorAll(".active")
+    progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%';
+   if(currentActive === 1){
+       prev.disabled = true;
+   }else if (currentActive === circles.length){
+       next.disabled = true;
+   }else{
+       prev.disabled = false;
+       next.disabled = false;
+   }
+
+}
